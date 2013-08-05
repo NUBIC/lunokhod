NUBIC/lunokhod

commit 71b9994c2bbe1e65e296c4f7f839e234e8f73c33

Neither the source line nor filename matter for node UUIDs.
Source lines and filenames are relevant to the _input_, not the
_survey_.  You can have two identical surveys with different formatting
and differing origin:

    # from one.rb
    survey "one" do
      section "foo" do
      end
    end

    # from curl https://foo.example.edu/surveys/one | bin/lunokhod-driver
    survey "one" do

      section "foo" do
      end
    end

Closes #3.

lib/lunokhod/ast.rb
@@ -7,7 +7,37 @@ module Lunokhod
   module Ast
     LUNOKHOD_V0_NAMESPACE = UUIDTools::UUID.parse('ecab6cb2-8755-11e2-8caf-b8f6b111aef5')
 
-    module Identifiable
+    # Every AST node has an identity and a source line number.
+    #
+    # Nodes are content-addressable
+    # -----------------------------
+    #
+    # A node's identity is a v5 UUID based on the Lunokhod namespace (see
+    # LUNOKHOD_V0_NAMESPACE), the node's attributes (i.e. struct members).
+    #
+    # Changes to a node's children propagate up all the way to the root.
+    # Therefore, survey equality can be checked by only comparing the UUIDs of
+    # two survey nodes.
+    #
+    #
+    # On line number tracking
+    # -----------------------
+    #
+    # Line number tracking is useful, but we don't want it to be part of a
+    # node's identity.
+    #
+    # Why?
+    #
+    # Because these two surveys have identical content:
+    #
+    # 1   survey "foo" do           survey "foo" do
+    # 2     section "bar" do
+    # 3     end                       section "bar" do
+    # 4   end                         end
+    # 5                             end
+    module AstNode
+      attr_accessor :line
+
       def identity
         @identity ||= ident(self)
       end
@@ -55,11 +85,12 @@ module Lunokhod
       end
     end
 
-    class Survey < Struct.new(:line, :name, :options, :sections, :translations, :source)
+    class Survey < Struct.new(:name, :options, :sections, :translations)
+      include AstNode
       include CommonOptions
-      include Identifiable
 
       attr_accessor :parent
+      attr_accessor :source
 
       def initialize(*)
         super
@@ -73,8 +104,8 @@ module Lunokhod
       end
     end
 
-    class Translation < Struct.new(:line, :lang, :path)
-      include Identifiable
+    class Translation < Struct.new(:lang, :path)
+      include AstNode
 
       attr_accessor :parent
 
@@ -83,9 +114,9 @@ module Lunokhod
       end
     end
 
-    class Section < Struct.new(:line, :tag, :name, :options, :questions)
+    class Section < Struct.new(:tag, :name, :options, :questions)
+      include AstNode
       include CommonOptions
-      include Identifiable
       include SurveyorTag
 
       attr_accessor :parent
@@ -103,9 +134,9 @@ module Lunokhod
       end
     end
 
-    class Label < Struct.new(:line, :text, :tag, :options, :dependencies)
+    class Label < Struct.new(:text, :tag, :options, :dependencies)
+      include AstNode
       include CommonOptions
-      include Identifiable
       include SurveyorTag
 
       attr_accessor :parent
@@ -121,9 +152,9 @@ module Lunokhod
       end
     end
 
-    class Question < Struct.new(:line, :text, :tag, :options, :answers, :dependencies)
+    class Question < Struct.new(:text, :tag, :options, :answers, :dependencies)
+      include AstNode
       include CommonOptions
-      include Identifiable
       include SurveyorTag
 
       attr_accessor :parent
@@ -140,9 +171,9 @@ module Lunokhod
       end
     end
 
-    class Answer < Struct.new(:line, :text, :type, :other, :tag, :validations, :options)
+    class Answer < Struct.new(:text, :type, :other, :tag, :validations, :options)
+      include AstNode
       include CommonOptions
-      include Identifiable
       include SurveyorTag
 
       attr_accessor :parent
@@ -167,8 +198,8 @@ module Lunokhod
       end
     end
 
-    class Dependency < Struct.new(:line, :rule, :conditions)
-      include Identifiable
+    class Dependency < Struct.new(:rule, :conditions)
+      include AstNode
       include RuleParsing
 
       attr_accessor :parent
@@ -188,8 +219,8 @@ module Lunokhod
       end
     end
 
-    class Validation < Struct.new(:line, :rule, :conditions)
-      include Identifiable
+    class Validation < Struct.new(:rule, :conditions)
+      include AstNode
       include RuleParsing
 
       attr_accessor :parent
@@ -209,9 +240,9 @@ module Lunokhod
       end
     end
 
-    class Group < Struct.new(:line, :tag, :name, :options, :questions, :dependencies)
+    class Group < Struct.new(:tag, :name, :options, :questions, :dependencies)
+      include AstNode
       include CommonOptions
-      include Identifiable
       include SurveyorTag
 
       attr_accessor :parent
@@ -228,9 +259,9 @@ module Lunokhod
       end
     end
 
-    class Condition < Struct.new(:line, :tag, :predicate)
+    class Condition < Struct.new(:tag, :predicate)
       extend Forwardable
-      include Identifiable
+      include AstNode
       include ConditionParsing
 
       attr_accessor :parent
@@ -271,8 +302,8 @@ module Lunokhod
       end
     end
 
-    class Grid < Struct.new(:line, :tag, :text, :questions, :answers)
-      include Identifiable
+    class Grid < Struct.new(:tag, :text, :questions, :answers)
+      include AstNode
 
       attr_accessor :parent
 
@@ -288,8 +319,8 @@ module Lunokhod
       end
     end
 
-    class Repeater < Struct.new(:line, :tag, :text, :questions, :dependencies)
-      include Identifiable
+    class Repeater < Struct.new(:tag, :text, :questions, :dependencies)
+      include AstNode
 
       attr_accessor :parent
 

lib/lunokhod/parser.rb
@@ -18,7 +18,7 @@ module Lunokhod
     end
 
     def survey(name, options = {}, &block)
-      survey = Ast::Survey.new(sline, name, options)
+      survey = build Ast::Survey, name, options
       survey.source = source
       @surveys << survey
 
@@ -30,7 +30,7 @@ module Lunokhod
 
     def translations(spec)
       spec.each do |lang, path|
-        translation = Ast::Translation.new(sline, lang, path)
+        translation = build Ast::Translation, lang, path
         translation.parent = @current_node
         @current_node.translations << translation
       end
@@ -38,7 +38,7 @@ module Lunokhod
 
     def dependency(options = {})
       rule = options[:rule]
-      dependency = Ast::Dependency.new(sline, rule)
+      dependency = build Ast::Dependency, rule
 
       # Does this apply to a question?  If not, we'll apply it to the current
       # node.
@@ -56,7 +56,7 @@ module Lunokhod
 
     def validation(options = {})
       rule = options[:rule]
-      validation = Ast::Validation.new(sline, rule)
+      validation = build Ast::Validation, rule
       validation.parent = @current_answer
       validation.parse_rule
       @current_answer.validations << validation
@@ -64,7 +64,7 @@ module Lunokhod
     end
 
     def _grid(tag, text, &block)
-      grid = Ast::Grid.new(sline, tag.to_s, text)
+      grid = build Ast::Grid, tag.to_s, text
       grid.parent = @current_node
       @current_node.questions << grid
 
@@ -76,7 +76,7 @@ module Lunokhod
     end
 
     def _repeater(tag, text, &block)
-      repeater = Ast::Repeater.new(sline, tag.to_s, text)
+      repeater = build Ast::Repeater, tag.to_s, text
       repeater.parent = @current_node
       @current_node.questions << repeater
 
@@ -88,14 +88,14 @@ module Lunokhod
     end
 
     def _label(tag, text, options = {})
-      question = Ast::Label.new(sline, text, tag.to_s, options)
+      question = build Ast::Label text, tag.to_s, options
       question.parent = @current_node
       @current_node.questions << question
       @current_question = question
     end
 
     def _question(tag, text, options = {})
-      question = Ast::Question.new(sline, text, tag.to_s, options)
+      question = build Ast::Question, text, tag.to_s, options
       question.parent = @current_node
       @current_node.questions << question
       @current_question = question
@@ -103,7 +103,7 @@ module Lunokhod
 
     def _answer(tag, t1, t2 = nil, options = {})
       text, type, other, options = _disambiguate_answer(t1, t2, options)
-      answer = Ast::Answer.new(sline, text, type, other, tag.to_s, [], options)
+      answer = build Ast::Answer, text, type, other, tag.to_s, [], options
 
       answer.parent = @current_question
       @current_question.answers << answer
@@ -132,9 +132,9 @@ module Lunokhod
 
     def _condition(label, *predicate)
       condition = if @current_dependency.is_a?(Ast::Dependency)
-                    Ast::DependencyCondition.new(sline, label, predicate)
+                    build Ast::DependencyCondition, label, predicate
                   elsif @current_dependency.is_a?(Ast::Validation)
-                    Ast::ValidationCondition.new(sline, label, predicate)
+                    build Ast::ValidationCondition, label, predicate
                   else
                     ::Kernel.raise "Unknown parent node #{@current_dependency.inspect} for condition"
                   end
@@ -145,7 +145,7 @@ module Lunokhod
     end
 
     def _group(tag, name = nil, options = {}, &block)
-      group = Ast::Group.new(sline, tag.to_s, name, options)
+      group = build Ast::Group, tag.to_s, name, options
       group.parent = @current_node
       @current_node.questions << group
 
@@ -157,7 +157,7 @@ module Lunokhod
     end
 
     def _section(tag, name, options = {}, &block)
-      section = Ast::Section.new(sline, tag.to_s, name, options)
+      section = build Ast::Section, tag.to_s, name, options
       section.parent = @current_node
       @current_node.sections << section
 
@@ -184,6 +184,10 @@ module Lunokhod
     # Current line in the survey.
     def sline
       ::Kernel.caller(1).detect { |l| l.include?(source) }.split(':')[1]
+    end
+
+    def build(node_class, *args)
+      node_class.new(*args).tap { |n| n.line = sline }
     end
 
     # Intercept DSL keywords that may be suffixed with tags.

spec/lunokhod/parser_spec.rb
@@ -5,6 +5,58 @@ module Lunokhod
     let(:parser) { Parser.new(data, 'fake') }
     let(:ast) { parser.surveys.first }
 
+    describe 'for surveys' do
+      let(:data) do
+        %q{
+          survey "fake" do
+            section "one" do
+            end
+          end
+        }
+      end
+
+      it 'ignores the source name when generating a survey UUID' do
+        p1 = Parser.new(data, 'p1')
+        p2 = Parser.new(data, 'p2')
+
+        p1.parse
+        p2.parse
+
+        p1.surveys.first.uuid.should == p2.surveys.first.uuid
+      end
+    end
+
+    describe 'node identification' do
+      let(:s1) do
+        %q{
+          survey "fake" do
+            section "one" do
+            end
+          end
+        }
+      end
+
+      let(:s2) do
+        %q{
+          survey "fake" do
+            # an empty line here
+            section "one" do
+            end
+          end
+        }
+      end
+
+      it 'is line-number independent' do
+        p1 = Parser.new(s1, 's1')
+        p2 = Parser.new(s2, 's2')
+
+        p1.parse
+        p2.parse
+
+        p1.surveys.first.uuid.should == p2.surveys.first.uuid
+      end
+    end
+
     describe 'for repeaters' do
       let(:data) do
         %q{

commit c756dd298d57cb6c7eab234128720c96e9102f83

Add a smoke test.
This is designed to prevent the sort of error I introduced in
71b9994c2bbe1e65e296c4f7f839e234e8f73c33 and corrected in
756710f76f7bf2fa1f16da5700d64a29ac59a8a0.

spec/smoke/example_parsing_spec.rb
@@ -1,0 +1,12 @@
+require 'spec_helper'
+
+describe Lunokhod do
+  it "can compile Surveyor's kitchen sink survey" do
+    driver = File.expand_path('../../../bin/lunokhod-driver', __FILE__)
+    survey = File.expand_path('../../../kitchen_sink_survey.rb', __FILE__)
+
+    system("#{driver} #{survey} > /dev/null 2>&1")
+
+    $?.should be_success
+  end
+end